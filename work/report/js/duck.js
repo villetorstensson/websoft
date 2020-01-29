443caa16b7ea79531c1936e810b3c4498d37fa2c
villetorstensson <villetorstensson@hotmail.com>
2020-01-29 11:48:00 +0100

    added duck to all pages

--- work/report/js/duck.js
+++ work/report/js/duck.js
@@ -3,17 +3,17 @@
 (function () {
     var element = document.getElementById("duck");
 
+   // element.addEventListener("click", function () {
+      //  element.innerHTML = element.innerHTML + "<p>" + element.offsetLeft + "</p>";
+      //  console.log("Duck clicked");
+
+   // });
+
     element.addEventListener("click", function () {
-        element.innerHTML = element.innerHTML + "<p>" + element.offsetLeft + "</p>";
-        console.log("Duck clicked");
-
-    });
-
-    element.addEventListener("mouseover", function () {
         element.style.left = element.offsetLeft + 20 + "px";
         console.log(element.style.left);
         console.log(element.offsetLeft);
-        console.log("Mouse over");
+        console.log("clicked");
 
     });
 
